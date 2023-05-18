7b882041335cc5879fd75c15914be78aaf0d79ad
Jhim Sebastian <76235294@continental.edu.pe>
2023-05-17 21:45:53 -0500

    Avance numero 3

--- test/PruebaOperacionesEnteros.py
+++ test/PruebaOperacionesEnteros.py
@@ -1,5 +1,35 @@
 import unittest
+from src.logica.OperacionesEnteros import OperacionesEnteros
 
 class PruebaOperacionesEnteros(unittest.TestCase):
-    def test_MCD(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.operacion = OperacionesEnteros([])
+
+    def tearDown(self):
+        self.operacion = None
+
+    def test_MCD_dosNumerosPositivos_retornaMCD(self):
+        # Arrange
+        numero1 = 18
+        numero2 = 24
+        resultadoEsperado = 6
+
+        # Do
+        resultadoActual = self.operacion.MCD(numero1, numero2)
+
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual)
+
+    def test_MCD_tresNumerosPositivos_retornaMCD(self):
+        # Arrange
+        numero1 = 24679
+        numero2 = 20387
+        numero3 = 16169
+        resultadoEsperado = 37
+        operacion = OperacionesEnteros([numero1, numero2, numero3])
+
+        # Do
+        resultadoActual = operacion.calcularMCD()
+
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual)
